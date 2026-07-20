31c0c49ffd9bc1bf1a0772659f0ad7d6379d7f9c
Hugo <hugo.percar53@gmail.com>
2026-07-20 16:34:35 -0600

    principal

--- src/app/componentes/cursos/cursos.ts
+++ src/app/componentes/cursos/cursos.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Curso } from '../curso/curso';
 
@@ -9,10 +9,48 @@ import { Curso } from '../curso/curso';
   templateUrl: './cursos.html',
   styleUrls: ['./cursos.css']
 })
-export class Cursos {
+export class Cursos implements OnInit {
 
   @Input() props!: {
     cursos: any[];
   };
 
+  currentIndex = 0;
+  itemsPerPage = 4;
+
+  ngOnInit() {
+    this.updateItemsPerPage();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateItemsPerPage();
+  }
+
+  updateItemsPerPage() {
+    if (typeof window !== 'undefined') {
+      this.itemsPerPage = window.innerWidth <= 768 ? 1 : 4;
+    }
+  }
+
+  get maxIndex(): number {
+    if (!this.props?.cursos) return 0;
+    return Math.max(0, this.props.cursos.length - this.itemsPerPage);
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.maxIndex; // vuelve al final
+    }
+  }
+
+  nextSlide() {
+    if (this.currentIndex < this.maxIndex) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0; // vuelve al inicio
+    }
+  }
 }
